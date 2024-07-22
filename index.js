d480607fbee6f8c21aaa29e64c51e75c9c40c482
Abhishek Dhakal <abhishekdhakal1826@gmail.com>
2024-07-22 18:49:26 +0545

    nodemon

--- index.js
+++ index.js
@@ -51,9 +51,12 @@ app.get("/users", (req, res) => {
 
 app.post("/users", (req, res) => {
   const body = req.body;
+  if (!body || !body.first_name || !body.last_name || !body.email) {
+    res.status(400).json({ msg: "all fiels are required" });
+  }
   users.push({ ...body, id: users.length + 1 });
   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.json({ status: "success", id: users.length });
+    return res.status(201).json({ status: "success", id: users.length });
   });
 });
 
