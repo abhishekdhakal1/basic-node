7d7b17d8dbb8a078376f6c8dbf71df9a95f201ee
Abhishek Dhakal <abhishekdhakal1826@gmail.com>
2024-07-24 23:04:21 +0545

    async flow control

---
+++ i1.js
@@ -0,0 +1,12 @@
+async1(function (input, result1) {
+    async2(function (result2) {
+      async3(function (result3) {
+        async4(function (result4) {
+          async5(function (output) {
+            //async .. flow control
+          });
+        });
+      });
+    });
+  });
+  
